ba28c131b6700e96c87fcfc0ffee6cb02b5e3997
Marialaura Bancheri <maryban@hotmail.it>
2017-09-19 18:12:06 +0200

    An error in the solution of the backward ODE was fixed. The numerical integrator was changed with the analytic solutio

--- src/main/java/travelTimesBack/ProbabilitiesBackward.java
+++ src/main/java/travelTimesBack/ProbabilitiesBackward.java
@@ -35,6 +35,7 @@ import org.geotools.feature.SchemaException;
 import org.jgrasstools.gears.libs.modules.JGTConstants;
 import org.jgrasstools.gears.libs.modules.JGTModel;
 import org.joda.time.DateTime;
+import org.joda.time.Days;
 import org.joda.time.Hours;
 import org.joda.time.format.DateTimeFormatter;
 
@@ -68,22 +69,23 @@ public class ProbabilitiesBackward extends JGTModel{
 	@In
 	public String inPathToStorage;
 
-	@Description("S: S value for the given time considered")
+	@Description("S: S value for the considered time step")
 	double S_t;
+	
 
-
-	@Description("S: S at time t+1")
+	@Description("S: S at time step t+1")
 	double S_t1;
 
 
 	@Description("[] inWaterStorageValues: time series array of all simulated S")
 	public Double [] inWaterStoragevalues;
+	
 
 	@Description("The path  of the simulated discharge")
 	@In
 	public String inPathToDischarge;
 
-	@Description("Q: Q value for the given time considered")
+	@Description("Q: Q value for the considered time step")
 	double Q;
 
 	@Description("[] inDischargeValues: time series array of all simulated Q")
@@ -93,7 +95,7 @@ public class ProbabilitiesBackward extends JGTModel{
 	@In
 	public String inPathToET;
 
-	@Description("ET: ET value for the given time considered")
+	@Description("ET: ET value for the considered time step")
 	double ET;
 
 
@@ -125,6 +127,10 @@ public class ProbabilitiesBackward extends JGTModel{
 	@Description("Last date of the simulation")
 	@In
 	public String tEndDate;
+	
+	@Description("Last date of the simulation")
+	@In
+	public int tTimestep;
 
 
 	@Description("String defining the SAS function chosen")
@@ -134,9 +140,6 @@ public class ProbabilitiesBackward extends JGTModel{
 	@Description("SAS  params")
 	@In
 	public double params [];
-
-	@Description("integration time")
-	double dt=1E-4;;
 
 
 	private DateTimeFormatter formatter = JGTConstants.utcDateFormatterYYYYMMDDHHMM;
@@ -158,14 +161,14 @@ public class ProbabilitiesBackward extends JGTModel{
 	int t;
 
 	@Description("threshold on the minimum value of the pdfs to be considered")
-	double threshold=0.1;
+	double threshold=0.0001;
 
 
 	@Description("SAS value at time t")
 	double SASvalue_t;
 
 
-	@Description("The output probabilities HashMap")
+	@Description("The output residence time backward probabilities matrix (t-ti)")
 	@Out
 	public HashMap<Integer, double[]> outHMPback= new HashMap<Integer, double[]>();;
 
@@ -173,40 +176,37 @@ public class ProbabilitiesBackward extends JGTModel{
 	@Description("The output probability at a given time-step")
 	double p_i;
 
-	@Description("The output probabilities HashMap")
+	@Description("The output travel time backward probabilities matrix (t-ti)")
 	@Out
 	public HashMap<Integer, double[]> outHMPQback= new HashMap<Integer, double[]>();
 
 
-	@Description("The output probabilities HashMap")
+	@Description("The output evapotranspiration time backward probabilities matrix (t-ti)")
 	@Out
 	public HashMap<Integer, double[]> outHMPETback= new HashMap<Integer, double[]>();
 
 
-	@Description("The output probabilities HashMap")
+	@Description("The output backward disharge matrix (t-ti)")
 	@Out
 	public HashMap<Integer, double[]> outHMQback= new HashMap<Integer, double[]>();
 
 
-	@Description("The output probabilities HashMap")
+	@Description("The output backward ET matrix (t-ti)")
 	@Out
 	public HashMap<Integer, double[]> outHMETback= new HashMap<Integer, double[]>();
 
 
-	@Description("The output HashMap with "
-			+ "the mean travel times values for each t and ti")
+	@Description("The output backward mean travel times matrix (t-ti)")
 	@Out
 	public HashMap<Integer, double[]> outHMQtt= new HashMap<Integer, double[]>();
 
 
-	@Description("The output HashMap with "
-			+ "the mean ET times values for each t and ti")
+	@Description("The output backward mean evapotranspiration times matrix (t-ti)")
 	@Out
 	public HashMap<Integer, double[]> outHMETtt= new HashMap<Integer, double[]>();
 
 
-	@Description("arraiFin is the final array needed for the rigth computationif the ID in the"
-			+ "output hashmap")
+	@Description("arraiFin is the array containing the t_i at the end of the computation")
 	ArrayList <Double> arrayFin= new ArrayList <Double>();
 
 
@@ -230,9 +230,13 @@ public class ProbabilitiesBackward extends JGTModel{
 
 		/** computation of the dimension of the array, given startDate and endDate */
 		DateTime start = formatter.parseDateTime(tStartDate);
-		DateTime end = formatter.parseDateTime(tEndDate);
-		dim=Hours.hoursBetween(start, end).getHours()+1;
+		DateTime end = formatter.parseDateTime(tEndDate);		
+		dim=(tTimestep==60)?Hours.hoursBetween(start, end).getHours()+1:Days.daysBetween(start, end).getDays()+1;
 
+		
+		/** deltaT is the difference between the beginning of the time series and 
+		 * the beginning of the computation, in the case the analysis starts from a different 
+		 * time-step */
 		if (tStartDateWaterBudget==null) deltaT=0;
 		else {DateTime startWaterBudget= formatter.parseDateTime(tStartDateWaterBudget);
 		deltaT=Hours.hoursBetween(startWaterBudget,start).getHours();}
@@ -246,48 +250,47 @@ public class ProbabilitiesBackward extends JGTModel{
 		if(t_i==0){	
 			inWaterStoragevalues=readData(inPathToStorage,ID);
 			inDischargevalues=readData(inPathToDischarge,ID);
-			inETvalues=	readData(inPathToET,ID);			
+			inETvalues=	(inPathToET==null)?inETvalues:readData(inPathToET,ID);			
 		}
 
 
-		/** initial conditions */
+		/** initial conditions */		
 		p_i=(inWaterStoragevalues[t_i+deltaT]>0)?precipitation/inWaterStoragevalues[t_i+deltaT]:0;
 		P.add(p_i);
 		pQ.add(distributionValue(0)*P.get(0));
 		pET.add(distributionValue(0)*P.get(0));
-		Q_out.add(pQ.get(0)*inDischargevalues[t_i+deltaT]);
-		ET_out.add(pET.get(0)*inETvalues[t_i+deltaT]);
+		Q_out.add(pQ.get(0)*inDischargevalues[t_i+deltaT]);		
+		if (inETvalues==null)ET_out.add(0.0);
+		else ET_out.add(pET.get(0)*inETvalues[t_i+deltaT]);		
 		QttV.add(0.0);
 		ETttV.add(0.0);
-
+		
 
 
 		/** loop for the computation of the probabilities and of the output fluxes
-		 * the loop starts continues until the value of the residence time pdf
-		 * is above the fixed threshold and the computations are made from the time
+		 * the loop continues until the value of the residence time pdf
+		 * is above the fixed threshold. The computation starts from time
 		 * step t=1, because of the initial conditions
 		 * */
 		t=1;
 
 		while(p_i>threshold& t<dim-t_i){
+			
 			Q=inDischargevalues[t+t_i+deltaT];
 			S_t=inWaterStoragevalues[t+t_i-1+deltaT];
 			S_t1=inWaterStoragevalues[t+t_i+deltaT];
-			ET=inETvalues[t+t_i+deltaT];
+			ET=(inETvalues==null)?0.0:inETvalues[t+t_i+deltaT];
 
 			/** StorageSelection value at time t */
 			SASvalue_t = distributionValue(t);
 			
-			//System.out.println(SASvalue_t);
 
-			P.add(computeP(SASvalue_t));
-			if (computeP(SASvalue_t)<0)
-				System.out.println("stop");
-			
+			P.add(computeP(SASvalue_t,p_i));
+						
 			pQ.add(computePQ(P.get(t),SASvalue_t));
 			
 			pET.add(computePET(P.get(t),SASvalue_t));
-			
+						
 			Q_out.add(computeQ(pQ.get(t)));
 
 			ET_out.add(computeET(pET.get(t)));
@@ -299,17 +302,17 @@ public class ProbabilitiesBackward extends JGTModel{
 			t++;
 		}
 
-		/** for each injection time the arrayFin is filled with a numeber,
-		 * needed for the ID of the output hashmap */
+		/** for each injection time the arrayFin is filled with t_i */
 		arrayFin.add((double)t_i);
 
 
+		System.out.println(t_i);
 		/** 
 		 * storage of results in hashmaps in which:
 		 * on the first column there re the injection times (date column)
 		 * and on the first row (value_t) are all the time step
 		 */
-		if (t_i<dim-1)storeResult(P,pQ,pET,Q_out,ET_out, QttV, ETttV);
+		if (t_i<dim-1)storeResult(P,pQ,pET,Q_out,ET_out,QttV,ETttV);
 		else {		
 			storeResult(arrayFin,arrayFin,arrayFin,arrayFin,arrayFin,arrayFin,arrayFin);   
 		}
@@ -332,6 +335,7 @@ public class ProbabilitiesBackward extends JGTModel{
 		CSTable Reader = DataIO.table(new File(inPath));
 		Double[] vector =DataIO.getColumnDoubleValues(Reader, "value_"+i);
 		return vector;
+		
 	}
 
 
@@ -346,30 +350,45 @@ public class ProbabilitiesBackward extends JGTModel{
 		SAS=SimpleDistributionFactory.createSAS(SAStype,params,((double)t+0.0001)/dim);		
 		return SAS.SASvalues(); 
 	}
-
-
+	
 	/**
 	 * Compute p: computation of the p_back.
 	 *
 	 * @param SASvalue_t the SAS value at time t, from the method distributionValue()
 	 * @return p_i the double value of the p backward
 	 */
-	public double computeP(double SASvalue_t){
+	public double computeP(double SASvalue_t, double p_i0){
 
-		/**Initialization of the ode*/
+		p_i= 1/S_t1*(-Q*SASvalue_t-ET*SASvalue_t)*p_i0+S_t*p_i0/S_t1;
+		p_i=(p_i<0)?0:p_i;
+		return p_i;		
+	}
+
+
+/*	*//** DOESN'T WORK
+	 * Compute p: computation of the p_back.
+	 *
+	 * @param SASvalue_t the SAS value at time t, from the method distributionValue()
+	 * @return p_i the double value of the p backward
+	 *//*
+	public double computePi(double SASvalue_t, double p_i0){
+
+		*//**Initialization of the ode*//*
 		FirstOrderDifferentialEquations pOde= new PbackODE(S_t, S_t1, Q, ET, SASvalue_t,
-				SASvalue_t, p_i, dt);
+				SASvalue_t, p_i0, dt);
 
-		/**Boundaries conditions*/
-		double[] y = new double[] { p_i, 0 };
+		*//**Boundaries conditions*//*
+		double[] y = new double[] { p_i0, 0 };
 
-		/**Choice of the ODE solver */	
+		*//**Choice of the ODE solver *//*	
 		SolverODE solver;
 		solver=SimpleIntegratorFactory.createSolver(m_solver, dt, pOde, y);
 		p_i=solver.integrateValues();
+		//System.out.println("p");
 
+		p_i=(p_i<0)?0:p_i;
 		return p_i;		
-	}
+	}*/
 
 
 	/**
@@ -394,6 +413,8 @@ public class ProbabilitiesBackward extends JGTModel{
 	public double computePET(double p_back,double SASvalue_t){
 		return p_back*SASvalue_t;		
 	}
+	
+
 
 	/**
 	 * ComputeQ: computation of the output discharge .
